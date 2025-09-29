bae7915410339b32bc6538666a486c6b9cd2c920
oanegros <oanegros@gmail.com>
2025-09-29 09:43:26 +0200

    try to fix windows segfault of pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,24 +9,27 @@ microscopynodes._test_register()
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
     import microscopynodes
-    # regrettably necessary, pytest segfaults if properties
-    # # with callback functions stay alive
+    regrettably necessary, pytest segfaults if properties
+    # with callback functions stay alive
 
-    # UPDATE_PROPS = [
-    #     'MiN_input_file',
-    #     'MiN_axes_order',
-    #     'MiN_channel_nr',
-    #     'MiN_reload',
-    # ]
-    # deleted = 0
-    # for prop in UPDATE_PROPS:
-    #     try:
-    #         delattr(bpy.types.Scene, prop)
-    #         deleted += 1
-    #     except:
-    #         print(f"{prop} not found")
+    UPDATE_PROPS = [
+        'MiN_input_file',
+        'MiN_axes_order',
+        'MiN_channel_nr',
+        'MiN_reload',
+    ]
+    deleted = 0
+    for prop in UPDATE_PROPS:
+        try:
+            delattr(bpy.types.Scene, prop)
+            deleted += 1
+        except:
+            print(f"{prop} not found")
 
-    microscopynodes.unregister()
+    try:
+        microscopynodes.unregister()
+    except Exception as e:
+        print(f"Warning during unregister: {e}")
     test_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp_test_data")
     try:
         if os.path.isdir(test_folder):
