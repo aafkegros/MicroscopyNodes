94141141ed9781b7881c7617183b47ceb04cf394
oanegros <oanegros@gmail.com>
2026-04-21 10:57:53 +0200

    active object change, remove some commented out code

--- microscopynodes/load.py
+++ microscopynodes/load.py
@@ -117,85 +117,42 @@ class Dataset():
         return
 
 
+# def load_blocking(dataset_model):
+#     # loads from the modal/threaded implementation
+#     # ch_dicts, (axes_order, pixel_size, size_px), cache_dir = params
+#     prev_active_obj = bpy.context.active_object
+#     scn = bpy.context.scene
 
-# def load_threaded(dataset_model):
-#     # try:
-#     if not bpy.context.scene.MiN_update_data:
-#         return dataset_model
-#     for ch in dataset_model.channels:
-#         for min_type, load in ch.visible_as.items():
-#             if load:
-#                 data_io = DataIOFactory(min_type)
-#                 file_constructors = data_io.generate_file_constructors(ch)
-#                 data_io.export_ch(ch, file_constructors)
-#     log('Loading objects to Blender')
-#     # except Exception as e:
-#     #     dataset_model.exception = str(e)
-#     return dataset_model
+#     # reads env variables
+#     base_coll = min_base_colls(Path(scn.MiN_input_file).stem[:50], scn.MiN_reload)    
 
-def load_blocking(dataset_model):
-    # loads from the modal/threaded implementation
-    # ch_dicts, (axes_order, pixel_size, size_px), cache_dir = params
-    prev_active_obj = bpy.context.active_object
-    scn = bpy.context.scene
+#     if scn.MiN_overwrite_background_color:
+#         set_background_color()
+#     if scn.MiN_overwrite_render_settings:
+#         set_render_settings()
 
-    # reads env variables
-    base_coll = min_base_colls(Path(scn.MiN_input_file).stem[:50], scn.MiN_reload)    
+#     # --- Prepare  container ---
+#     container = scn.MiN_reload
+#     objs = parse_reload(container)
 
-    if scn.MiN_overwrite_background_color:
-        set_background_color()
-    if scn.MiN_overwrite_render_settings:
-        set_render_settings()
+#     if container is None:
+#         bpy.ops.object.empty_add(type="PLAIN_AXES")
+#         container = bpy.context.view_layer.objects.active
+#         container.name = Path(scn.MiN_input_file).stem[:50]
 
-    # --- Prepare  container ---
-    container = scn.MiN_reload
-    objs = parse_reload(container)
 
-    if container is None:
-        bpy.ops.object.empty_add(type="PLAIN_AXES")
-        container = bpy.context.view_layer.objects.active
-        container.name = Path(scn.MiN_input_file).stem[:50]
-
-    # -- axes, slice cube and scales -- 
-    # scale, scale_factor = parse_scale(size_px, pixel_size, objs) 
-    # loc = parse_loc(scale, size_px, container)
-    # axes_obj = load_axes(size_px, pixel_size, scale, scale_factor, axes_obj=objs[min_keys.AXES], container=container)
-    # slice_cube = load_slice_cube(size_px, scale, scale_factor, container, slicecube=objs[min_keys.SLICECUBE])
-
-    # for min_type in [min_keys.VOLUME, min_keys.SURFACE, min_keys.LABELMASK]:
-    #     if not any([ch[min_type] for ch in ch_dicts]) and objs[min_type] is None:
-    #         continue
-    #     ch_obj = ChannelObjectFactory(min_type, objs[min_type], scale)
-
-    #     for ch in ch_dicts:
-    #         if ch[min_type] and scn.MiN_update_data:
-    #             file_constructors = DataIOFactory(min_type).generate_file_constructors(ch, cache_dir)
-    #             ch.metadata[min_type] = DataIOFactory(min_type).get_metadata(file_constructors)
-    #             ch_obj.update_ch_data(ch, file_constructors)
-    #         if scn.MiN_update_settings:
-    #             ch_obj.update_ch_settings(ch)
-    #         ch_obj.set_parent_and_slicer(container, slice_cube, ch)
-
-    # container.location = loc
-    
-    # # -- wrap up --
-    # collection_deactivate_by_name('cache')
-
-    # if scn.frame_current < scn.MiN_load_start_frame or scn.frame_current > scn.MiN_load_end_frame:
-    #     scn.frame_set(scn.MiN_load_start_frame)
-
-    try:
-        if prev_active_obj is not None:
-            prev_active_obj.select_set(True)
-            bpy.context.view_layer.objects.active = prev_active_obj
-    except:
-        pass
-    # after first load this should not be used again, to prevent overwriting user values
-    scn.MiN_reload = container
-    scn.MiN_overwrite_render_settings = False
-    scn.MiN_enable_ui = True
-    log('')
-    return
+#     try:
+#         if prev_active_obj is not None:
+#             prev_active_obj.select_set(True)
+#             bpy.context.view_layer.objects.active = prev_active_obj
+#     except:
+#         pass
+#     # after first load this should not be used again, to prevent overwriting user values
+#     scn.MiN_reload = container
+#     scn.MiN_overwrite_render_settings = False
+#     scn.MiN_enable_ui = True
+#     log('')
+#     return
 
 
 
