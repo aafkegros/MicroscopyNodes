02057758ffc22e29a872d4d3a72e979fe5430b67
oanegros <oanegros@gmail.com>
2026-04-24 15:15:41 +0200

    join grids

--- microscopynodes/min_nodes/geo_nodes/__init__.py
+++ microscopynodes/min_nodes/geo_nodes/__init__.py
@@ -1,10 +1,12 @@
 from .import_microscopy_meshes import import_microscopy_meshes_node_group
 from .import_microscopy_volume import import_microscopy_volume_node_group
+from .join_grids import join_grids_node_group
 
 
 NODE_GROUPS = {
     "Import Microscopy Meshes": import_microscopy_meshes_node_group,
     "Import Microscopy Volume": import_microscopy_volume_node_group,
+    "Join Grids": join_grids_node_group,
 }
 
 
