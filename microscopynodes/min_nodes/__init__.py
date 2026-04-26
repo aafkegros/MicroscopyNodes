0bc76d9dbce9722493500cd7d0d8046b1005e3f9
oanegros <oanegros@gmail.com>
2026-04-26 11:21:10 +0200

    clean up filepaths nodes

--- microscopynodes/min_nodes/__init__.py
+++ microscopynodes/min_nodes/__init__.py
@@ -1,8 +1,10 @@
-from .nodeScale import scale_node_group
-from .nodeCrosshatch import crosshatch_node_group
-from .nodeGridVerts import grid_verts_node_group
-from .nodeScaleBox import scalebox_node_group
-from .nodeSliceCube import slice_cube_node_group
+from .geo_nodes import (
+    crosshatch_node_group,
+    grid_verts_node_group,
+    scale_node_group,
+    scalebox_node_group,
+)
+from .shader_nodes import slice_cube_node_group
 from .geo_nodes import geometry_node_group
 
 from . import shader_nodes
