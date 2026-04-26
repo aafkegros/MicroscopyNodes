299b3a7a60819de86a913e7b43051226bf04af09
oanegros <oanegros@gmail.com>
2026-04-26 09:09:09 +0200

    postload select

--- microscopynodes/ui/ops.py
+++ microscopynodes/ui/ops.py
@@ -15,6 +15,29 @@ from ..load import Scene, Dataset
 from ..parse_inputs import parse_blender_ui
 
 
+def select_post_load_object(context, dataset, previous_active_obj):
+    try:
+        if previous_active_obj is not None and previous_active_obj.name in bpy.data.objects:
+            previous_active_obj.select_set(True)
+            context.view_layer.objects.active = previous_active_obj
+            return
+    except Exception:
+        pass
+
+    for min_obj in (dataset.volume, dataset.surface, dataset.labelmask, dataset.axes, dataset.slicecube):
+        if min_obj is None:
+            continue
+        obj = min_obj.object
+        if obj is None:
+            continue
+        try:
+            obj.select_set(True)
+            context.view_layer.objects.active = obj
+            return
+        except Exception:
+            continue
+
+
 class TifLoadOperator(bpy.types.Operator):
     """ Load a microscopy dataset. Resaves your data into vdb (volume) and abc (mask) formats into Cache Folder"""
     bl_idname ="microscopynodes.load"
@@ -35,13 +58,9 @@ class TifLoadOperator(bpy.types.Operator):
                     return {"CANCELLED"}
                 context.window_manager.event_timer_remove(self._timer)
                 Scene.from_blender_ui(context)
-                Dataset(holder=context.scene.MiN_reload).set_state(self.dataset_model)
-                try:
-                    if prev_active_obj is not None:
-                        prev_active_obj.select_set(True)
-                        bpy.context.view_layer.objects.active = prev_active_obj
-                except:
-                    pass
+                dataset = Dataset(holder=context.scene.MiN_reload)
+                dataset.set_state(self.dataset_model)
+                select_post_load_object(context, dataset, self.prev_active_obj)
                 return {'FINISHED'}
             if not self.thread.is_alive():
                 self.thread = None # update UI for one timer-round
@@ -85,7 +104,9 @@ class TifLoadBackgroundOperator(bpy.types.Operator):
         dataset_model = parse_blender_ui()
         dataset_model.make_local_files()
         Scene.from_blender_ui(context)
-        Dataset(holder=context.scene.MiN_reload).set_state(dataset_model)
+        dataset = Dataset(holder=context.scene.MiN_reload)
+        dataset.set_state(dataset_model)
+        select_post_load_object(context, dataset, context.active_object)
         return {'FINISHED'}
 
 
